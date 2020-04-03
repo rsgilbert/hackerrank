229f8e4e98a14d33cb32272b2e5a3a80d0ecea34
Gilbert <gssenyonjo@gmail.com>
2020-04-03 18:15:27 +0300

    finished EnumerateInput

---
+++ src/EnumerateInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EnumerateInput {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int count = 1;
+        while(scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.printf("%d %s\n", count, line);
+            count++;
+        }
+    }
+}
