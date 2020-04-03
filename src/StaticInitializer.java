61bdd32d16ee61236c726cd199bda89855a7daed
Gilbert <gssenyonjo@gmail.com>
2020-04-03 18:55:22 +0300

    Finished StaticInitializer

---
+++ src/StaticInitializer.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StaticInitializer {
+    static int B, H;
+    static boolean flag;
+    static {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        flag = B > 0 && H > 0;
+        if(!flag) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+}
