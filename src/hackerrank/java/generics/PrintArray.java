d1b68e663727e58226d3cd4db6d8323afc3d292a
Gilbert <gssenyonjo@gmail.com>
2020-04-06 16:17:04 +0300

    Did Generics lesson. Finished PrintArray

---
+++ src/hackerrank/java/generics/PrintArray.java
@@ -0,0 +1,32 @@
+package hackerrank.java.generics;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class PrintArray {
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+
+    }
+}
+
+
+class Printer {
+    public <T> void printArray(T[] tArray) {
+        for(T t: tArray) System.out.println(t);
+    }
+}
---
+++ src/hackerrank/java/learning/Generics.java
@@ -0,0 +1,18 @@
+package hackerrank.java.learning;
+
+public class Generics {
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+    }
+}
+
+/**
+ * Generic version of the Box class
+ * @param <T> the type of the value being boxed
+ */
+class Box<T> {
+    private T t;
+
+    public void set(T t) { this.t = t; }
+    public T get() { return t; }
+}
