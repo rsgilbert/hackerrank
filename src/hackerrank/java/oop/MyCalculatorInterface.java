4eed8dafb3fe140f4dd8c3d05a38afc8e2fe3528
Gilbert <gssenyonjo@gmail.com>
2020-04-07 21:08:25 +0300

    used streams in MyCalculator to calculator divisor_sum

--- src/hackerrank/java/oop/MyCalculatorInterface.java
+++ src/hackerrank/java/oop/MyCalculatorInterface.java
@@ -1,24 +1,23 @@
 package hackerrank.java.oop;
 
 import java.util.*;
-interface AdvancedArithmetic{
+import java.util.stream.IntStream;
+
+interface AdvancedArithmetic {
     int divisor_sum(int n);
 }
 
 //Write your code here
 class MyCalculator implements AdvancedArithmetic {
     public int divisor_sum(int n) {
-        int sum = 0;
-        for(int i = 1; i <= n; i ++) {
-            if(n % i == 0) {
-                sum = Integer.sum(sum, i);
-            }
-        }
-        return sum;
+        return IntStream.rangeClosed(1, n)
+                .filter(i -> n % i == 0)
+                .sum();
     }
 }
-class Solution{
-    public static void main(String []args){
+
+class Solution {
+    public static void main(String[] args) {
         MyCalculator my_calculator = new MyCalculator();
         System.out.print("I implemented: ");
         ImplementedInterfaceNames(my_calculator);
@@ -27,12 +26,13 @@ class Solution{
         System.out.print(my_calculator.divisor_sum(n) + "\n");
         sc.close();
     }
+
     /*
      *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
      */
-    static void ImplementedInterfaceNames(Object o){
+    static void ImplementedInterfaceNames(Object o) {
         Class[] theInterfaces = o.getClass().getInterfaces();
-        for (int i = 0; i < theInterfaces.length; i++){
+        for (int i = 0; i < theInterfaces.length; i++) {
             String interfaceName = theInterfaces[i].getName();
             System.out.println(interfaceName);
         }
