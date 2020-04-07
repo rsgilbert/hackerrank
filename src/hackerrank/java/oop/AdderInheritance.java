e96132db6972e335b2164b8bce14bd78408c5742
Gilbert <gssenyonjo@gmail.com>
2020-04-07 20:32:21 +0300

    finished MyCalculatorInterface.java

---
+++ src/hackerrank/java/oop/AdderInheritance.java
@@ -0,0 +1,26 @@
+package hackerrank.java.oop;
+
+//Write your code here
+
+class Arithmetic {
+    public int add(int a1, int a2) {
+        return Integer.sum(a1, a2);
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
+
+public class AdderInheritance {
+    public static void main(String []args){
+        // Create a new Adder object
+        Adder a = new Adder();
+
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
+    }
+}
