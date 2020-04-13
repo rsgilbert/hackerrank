1a0fb08c0f3a31b0e3bb57ed5246318d2bf32937
Gilbert <gssenyonjo@gmail.com>
2020-04-13 15:28:50 +0300

    Completed research and practice on difference array, prefix sum array, their relationships with calculus as well as their applications in problem solving and algorithms

--- src/hackerrank/learning/DifferenceArray.java
+++ src/hackerrank/learning/DifferenceArray.java
@@ -19,6 +19,11 @@ public class DifferenceArray {
         return diffArray;
     }
 
+    public static void printArray(int[] array) {
+        for(int i: array) System.out.print(i + " ");
+        System.out.println();
+    }
+
     /**
      * we first append c onto the front of the array, and then replace each element with the sum of itself and all the elements preceding it.
      */
@@ -36,29 +41,81 @@ public class DifferenceArray {
         int[] prefixSumArray = computePrefixSumArray(0, array);
         // recall prefixSumArray has one more item so we must increment endIdx
         // We do not increment startIdx as well because we are subtracting sums before startIdx
-        endIdx ++;
+        endIdx++;
 
         return prefixSumArray[endIdx] - prefixSumArray[startIdx];
     }
 
+    /**
+     * Problem: Counting Subsequences
+     * Given an array of integers S, find the number of contiguous subsequences of the array that sum to 47.
+     */
+    public static int numberOfContiguousSubsequences(int[] array, int sum) {
+        int maxCount = 0;
+        int[] prefixSumArray = computePrefixSumArray(0, array);
+
+        for (int i = 1; i < prefixSumArray.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (prefixSumArray[i] - prefixSumArray[j] == sum) {
+                    maxCount++;
+                }
+            }
+        }
+        return maxCount;
+    }
+
+    /**
+     * Use of difference arrays
+     * Updating range
+     */
+    public static int[] computePlateau(int[] array, int startIdx, int endIdx, int update) {
+        int[] diffArray = computeDifferenceArray(array);
+//        We substract 1 to compensate for the decrease in array length
+        int newStartIdx = startIdx - 1;
+//        starting difference goes up because the range values are gaining an increase.
+        diffArray[newStartIdx] += update;
+
+        // we do not subtract 1 from endIdx because we are interested in the difference between the
+        // last value in the range and the next value after the range.
+        // end difference goes down because the difference decreases at the end of the range.
+        diffArray[endIdx] -= update;
+
+        return computePrefixSumArray(array[0], diffArray);
+    }
 
     public static void main(String[] args) {
-        int[] A = new int[]{9, 2, 6, 3, 7, 5, 0, 7};
-        int[] diffArray = computeDifferenceArray(A);
-        int[] prefixSumArray = computePrefixSumArray(0, A);
+//        int[] A = new int[]{9, 2, 6, 3, 7, 5, 0, 7};
+//        int[] diffArray = computeDifferenceArray(A);
+//        int[] prefixSumArray = computePrefixSumArray(0, A);
+//
+//        for (int k : diffArray) System.out.print(k + " ");
+//        System.out.println();
+//
+//        for (int k : prefixSumArray) System.out.print(k + " ");
+//        System.out.println();
 
-        for (int k : diffArray) System.out.print(k + " ");
+//        int[] cancelOut1 = computeDifferenceArray(computePrefixSumArray(3, A));
+//        for (int k : cancelOut1) System.out.print(k + " ");
+//        System.out.println();
+
+//        int sumBetweenPositions = computeSumBetweenPositions(A, 1, 3);
+//        System.out.println(sumBetweenPositions);
+
+        // Continuous subsequences
+         int[] myArray = new int[] { 9, 5, 6, 3, -3, 3 };
+         int numberOfContinuousSubseqs = numberOfContiguousSubsequences(myArray, 9);
+        System.out.println("Number of continuous subsequences is " + numberOfContinuousSubseqs);
+
+        int[] plateauArray = computePlateau(myArray, 2, 4, 3);
+        System.out.println("Plateau");
+        printArray(plateauArray);
+
+        // Calculus
+        // Fundamental Theorem of Calculus
+        // Integral of D(A) ie P(0, D(A))
+//        System.out.println("P(0, D(A))");
+//        int[] ftc = computePrefixSumArray(0, diffArray);
+//        for (int f : ftc) System.out.print(f + " ");
         System.out.println();
-
-        for (int k : prefixSumArray) System.out.print(k + " ");
-        System.out.println();
-
-        int[] cancelOut1 = computeDifferenceArray(computePrefixSumArray(3, A));
-        for (int k : cancelOut1) System.out.print(k + " ");
-        System.out.println();
-
-        int sumBetweenPositions = computeSumBetweenPositions(A, 1, 3);
-        System.out.println(sumBetweenPositions);
-
     }
 }
